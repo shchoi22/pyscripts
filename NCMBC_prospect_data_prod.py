2e3a93aa9a996a65f713b7e1f07edbc3a071bd15
Stan Choi <shchoi22@gmail.com>
2014-03-26 14:23:55 -0500

    updated script

--- NCMBC_prospect_data_prod.py
+++ NCMBC_prospect_data_prod.py
@@ -4,6 +4,8 @@ from email.MIMEBase import MIMEBase
 from email.MIMEText import MIMEText
 from email import Encoders
 import os
+import sys
+import traceback
 import psycopg2 as pg
 import pandas as pd
 from pandas import ExcelWriter
@@ -33,28 +35,48 @@ def mail(to, subject, text, attach):
    # Should be mailServer.quit(), but that crashes...
    mailServer.close()
 
-con = pg.connect("host={0} dbname={1} user={2} password={3}".format(config.chartio_host,config.chartio_db,config.chartio_id,config.chartio_pwd))
-cur = con.cursor()
+try:
+   con = pg.connect("host={0} dbname={1} user={2} password={3}".format(config.chartio_host,config.chartio_db,config.chartio_id,config.chartio_pwd))
+   cur = con.cursor()
 
-f = open("/home/schoi/scripts/NCMBC_prospect_without_app_list_3.20.14.sql",'r')
-query = "".join(i for i in f.read() if ord(i)<128)
-cur.execute(query)
-data_prospect = pd.DataFrame(cur.fetchall(),columns=[desc[0] for desc in cur.description])
+   f = open("/home/schoi/scripts/NCMBC_prospect_without_app_list_3.20.14.sql",'r')
+   query = "".join(i for i in f.read() if ord(i)<128)
+   cur.execute(query)
+   data_prospect = pd.DataFrame(cur.fetchall(),columns=[desc[0] for desc in cur.description])
 
-f2 = open("/home/schoi/scripts/NCMBC_prospect_approved_list_3.20.14.sql",'r')
-query2 = "".join(i for i in f2.read() if ord(i)<128)
-cur.execute(query2)
-data_approved = pd.DataFrame(cur.fetchall(),columns=[desc[0] for desc in cur.description])
+   f2 = open("/home/schoi/scripts/NCMBC_prospect_approved_list_3.20.14.sql",'r')
+   query2 = "".join(i for i in f2.read() if ord(i)<128)
+   cur.execute(query2)
+   data_approved = pd.DataFrame(cur.fetchall(),columns=[desc[0] for desc in cur.description])
 
-cur.close()
-con.close()
+   cur.close()
+   con.close()
 
-writer = ExcelWriter('output.xlsx')
-data_prospect.to_excel(writer,'prospects_without_app')
-data_approved.to_excel(writer,'approved_prospects')
-writer.save()
+   writer = ExcelWriter('output.xlsx')
+   data_prospect.to_excel(writer,'prospects_without_app')
+   data_approved.to_excel(writer,'approved_prospects')
+   writer.save()
 
-mail(config.recip,
-   "Pangea Prospects List (NCMBC)",
+   mail(config.recip_external,
+   "Pangea Properties + NCMBC Prospects",
    "Excel file contains prospects with no app and approved prospects in separate sheets",
    "output.xlsx")
+except:
+    exc_type, exc_value, exc_traceback = sys.exc_info()
+    lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
+    error_log = ''.join('!! ' + line for line in lines)
+
+    #sending email of error log
+    sender = config.gmail_id
+    receivers = [config.recip]
+    message = """From: Linux Box <{0}>\nTo:<{1}>\nSubject: ERROR LOG-FAIL TO WRITE {2}\n\nError message: {3}.""".format(config.gmail_id,config.recip,'NCMBC_prospect_data_prod.py',error_log)
+    try:
+        session = smtplib.SMTP('smtp.gmail.com',587)
+        session.ehlo()
+        session.starttls()
+        session.ehlo()
+        session.login(sender,config.gmail_pwd)
+        session.sendmail(sender, receivers, message)
+        print "Successfully sent email"
+    except smtplib.SMTPException:
+        print "Error: unable to send email"
